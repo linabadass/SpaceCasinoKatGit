e7e6ebe04f5b2b074dff4209900d5ab0a59ccddd
linabadass <linanaim@hotmail.com>
2020-02-13 13:02:43 +0000

    Add files via upload

---
+++ Test Suites/Live suite/DotCom Sanity test.ts
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DotCom Sanity test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>04ee844f-bda0-451f-94f9-51ae144ccbd7</testSuiteGuid>
+   <testCaseLink>
+      <guid>9df1ad31-7678-432f-a164-abdc181d0a79</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DotCom Test Cases/DotCom Account Activities</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f9399fb4-2162-4644-9a34-4d97c7c763fe</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DotCom Test Cases/DotCom Footer links</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b92c48e9-374c-45d3-a4ca-cf98fa4e278f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DotCom Test Cases/DotCom Forgot Password</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e99451ea-38cb-4f4d-beb2-c74dca2bcca7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DotCom Test Cases/DotCom Header Menus</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>54ed54f4-38fc-4454-97bf-fc9504b95d9b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DotCom Test Cases/DotCom links in help section</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c73ea32e-9566-4d65-87b0-cc6ec6dd6601</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DotCom Test Cases/DotCom Sign in and out</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/Live suite/Live Sanity test.ts
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Live Sanity test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>ec8ac547-b51c-495c-ac03-6712ab460ffd</testSuiteGuid>
+   <testCaseLink>
+      <guid>16275eda-08db-4dc7-bb83-d61bbc30a2a9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Live Test Cases/Live Account Activities</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>39ffea57-7ae7-4dd9-85bc-f1f99a35971c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Live Test Cases/Live Footer links</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>41ad77f5-b3b5-4f1e-a933-309f8a6d5c49</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Live Test Cases/Live Forgot Password</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4b1ce07b-f4c4-4e33-a2b8-11ff2b24c081</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Live Test Cases/Live Header Menus</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>dea1d192-43c8-4505-b71a-4871dfa9cf9d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Live Test Cases/Live links in help section</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>623ce810-ab56-415e-ad41-9c275fb0618d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Live Test Cases/Live Sign in and out</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
